d5ad944486385dba7277e0f5f9b6d8dafe611bca
yatziel <ylopez@asmedia.edu.mx>
2023-08-30 01:58:37 -0600

    Add CSS and JS files and first styles and functions

---
+++ app.js
@@ -0,0 +1,12 @@
+const menuButton = document.getElementById('menuButton');
+const menuMobile = document.getElementById('menuMobile');
+
+menuButton.addEventListener('click', () => {
+	menuMobile.classList.toggle('menu-show');
+});
+
+document.querySelectorAll('.menu-link').forEach((n) =>
+	n.addEventListener('click', () => {
+		menuMobile.classList.remove('menu-show');
+	})
+);
