8a16947941df984738bbc0c68f7d2ee88e0def74
yatziel <ylopez@asmedia.edu.mx>
2023-08-31 02:20:14 -0600

    Create the styles and functionality for the mobile version of the navbar.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,5 @@
 'use strict';
-console.log('🎈 Beep Beep Richie');
+var menuButton = document.getElementById('menuButton');
+var menuMobile = document.getElementById('menuMobile');
+menuButton.addEventListener('click', function () { menuMobile.classList.toggle('menu-show'); });
+document.querySelectorAll('.header-a').forEach(function (n) { return n.addEventListener('click', function () { menuMobile === null || menuMobile === void 0 ? void 0 : menuMobile.classList.remove('menu-show'); }); });
--- ts/app.ts
+++ ts/app.ts
@@ -1,17 +1,8 @@
 'use strict';
 
-console.log('🎈 Beep Beep Richie');
-
-/* const menuButton = document.getElementById('menuButton');
+const menuButton = document.getElementById('menuButton');
 const menuMobile = document.getElementById('menuMobile');
 
-menuButton.addEventListener('click', () => {
-	menuMobile.classList.toggle('menu-show');
-});
+menuButton.addEventListener('click', () => { menuMobile.classList.toggle('menu-show') });
 
-document.querySelectorAll('.menu-link').forEach((n) =>
-	n.addEventListener('click', () => {
-		menuMobile.classList.remove('menu-show');
-	})
-);
- */
+document.querySelectorAll('.header-a').forEach( (n) => n.addEventListener('click', () => { menuMobile?.classList.remove('menu-show') }) );
