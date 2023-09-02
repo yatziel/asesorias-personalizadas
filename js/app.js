5443502463c88f6a1a21c179f9822f863bf86bad
Yatziel Ilhuicatl <ylopez@asmedia.edu.mx>
2023-09-01 18:20:47 -0600

    Create structure  and soft styles for advisory section

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-const menuButton = document.getElementById('menuButton');
-const menuMobile = document.getElementById('menuMobile');
-menuButton.addEventListener('click', () => { menuMobile.classList.toggle('menu-show'); });
-document.querySelectorAll('.header-a').forEach((n) => n.addEventListener('click', () => { menuMobile === null || menuMobile === void 0 ? void 0 : menuMobile.classList.remove('menu-show'); }));
+var menuButton = document.getElementById('menuButton');
+var menuMobile = document.getElementById('menuMobile');
+menuButton.addEventListener('click', function () { menuMobile.classList.toggle('menu-show'); });
+document.querySelectorAll('.header-a').forEach(function (n) { return n.addEventListener('click', function () { menuMobile === null || menuMobile === void 0 ? void 0 : menuMobile.classList.remove('menu-show'); }); });
