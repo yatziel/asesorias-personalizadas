22ce4ae69ed81e9c9adb1c8a3e3f369f21a33b10
Yatziel Ilhuicatl <ylopez@asmedia.edu.mx>
2023-09-21 16:57:41 -0600

    Adjust general

--- js/app.js
+++ js/app.js
@@ -1,11 +1,83 @@
 'use strict';
 
-/* const menuButton = document.getElementById('menuButton');
-const menuMobile = document.getElementById('menuMobile');
+/* Button Toggle Navbar Mobile */
+var menuButton = document.getElementById('menuButton');
+var menuMobile = document.getElementById('menuMobile');
+menuButton.addEventListener('click', function () {
+  menuMobile.classList.toggle('menu-show');
+});
+document.querySelectorAll('.header-a').forEach(function (n) {
+  return n.addEventListener('click', function () {
+    menuMobile.classList.remove('menu-show');
+  });
+});
 
-menuButton.addEventListener('click', () => { menuMobile.classList.toggle('menu-show') });
+/* Hero Transition: Titles and Images */
 
-document.querySelectorAll('.header-a').forEach( (n) => n.addEventListener('click', () => { menuMobile?.classList.remove('menu-show') }) ); */
+var heroTitles = document.querySelectorAll('.hero-title');
+var heroImages = document.querySelectorAll('.hero-image');
+var i = 0;
+setInterval(function () {
+  heroTitles[i].classList.remove('active');
+  heroImages[i].classList.remove('active');
+  i++;
+  if (i >= heroImages.length) {
+    i = 0;
+  }
+  heroTitles[i].classList.add('active');
+  heroImages[i].classList.add('active');
+}, 3000);
+
+/* Programs Slider */
+
+var program = document.querySelectorAll('.advisories-program');
+var buttonPrev = document.getElementsByClassName('btn-left');
+var buttonNext = document.getElementsByClassName('btn-rigth');
+
+/* let p: number = 0;
+
+function nextProgram() {
+	program[p].classList.remove('active')
+	p++
+
+	if(p >= program.length) { p = 0 }
+
+	program[p].classList.add('active');
+}
+ */
+
+var p = 0;
+setInterval(function () {
+  program[p].classList.remove('active');
+  p++;
+  if (p >= program.length) {
+    p = 0;
+  }
+  program[p].classList.add('active');
+}, 3000);
+
+/* Testimonials Transition */
+
+var testSlider = document.querySelector('.testimonials-slides');
+var testSlide = document.querySelectorAll('.testimonials-slide');
+var testSlideLast = testSlide[testSlide.length - 1];
+testSlider.insertAdjacentElement('afterbegin', testSlideLast);
+function nextSilder() {
+  var testSlideFirst = document.querySelectorAll('.testimonials-slide')[0];
+  testSlider.style.marginLeft = '-200%';
+  testSlider.style.transition = 'all 1s';
+  setTimeout(function () {
+    testSlider.style.transition = 'none';
+    testSlider.insertAdjacentElement('beforeend', testSlideFirst);
+    testSlider.style.marginLeft = '-100%';
+  }, 1000);
+}
+setInterval(function () {
+  nextSilder();
+}, 7000);
+
+/* Toggle FAQs */
+
 var acc = document.getElementsByClassName('faqs-question');
 for (var _i = 0; _i < acc.length; _i++) {
   acc[_i].addEventListener('click', function () {
@@ -18,23 +90,4 @@ for (var _i = 0; _i < acc.length; _i++) {
       panel.style.maxHeight = panel.scrollHeight + 'px';
     }
   });
-<<<<<<< HEAD
 }
-var heroTitles = document.querySelectorAll('.hero-title');
-var heroImages = document.querySelectorAll('.hero-image');
-setInterval(function () {
-  heroTitles[i].classList.remove('active');
-  heroImages[i].classList.remove('active');
-  i++;
-  if (i >= heroTitles.length) {
-    i = 0;
-  }
-  if (i >= heroImages.length) {
-    i = 0;
-  }
-  heroTitles[i].classList.add('active');
-  heroImages[i].classList.add('active');
-}, 3000);
-=======
-}
->>>>>>> dc25fb369eb30e85e6d20d4de9eeec6c47a878a7
--- ts/app.ts
+++ ts/app.ts
@@ -1,12 +1,98 @@
 'use strict';
 
-/* const menuButton = document.getElementById('menuButton');
-const menuMobile = document.getElementById('menuMobile');
+/* Button Toggle Navbar Mobile */
 
-menuButton.addEventListener('click', () => { menuMobile.classList.toggle('menu-show') });
+const menuButton = (document.getElementById('menuButton') as HTMLButtonElement)
+const menuMobile = (document.getElementById('menuMobile')	as HTMLElement)
 
-document.querySelectorAll('.header-a').forEach( (n) => n.addEventListener('click', () => { menuMobile?.classList.remove('menu-show') }) ); */
+menuButton.addEventListener('click', () : void => { menuMobile.classList.toggle('menu-show') })
+document.querySelectorAll('.header-a').forEach( (n) => n.addEventListener('click', () : void  => { menuMobile.classList.remove('menu-show') }) )
 
+/* Hero Transition: Titles and Images */
+
+const heroTitles = (document.querySelectorAll('.hero-title') as NodeList)
+const heroImages = (document.querySelectorAll('.hero-image') as NodeList)
+
+let i : number = 0
+
+setInterval ( function () {
+		heroTitles[i].classList.remove('active')
+		heroImages[i].classList.remove('active')
+		i++
+
+		if (i >= heroImages.length) { i = 0 }
+
+		heroTitles[i].classList.add('active')
+		heroImages[i].classList.add('active')
+
+	}, 3000
+)
+
+
+/* Programs Slider */
+
+const program = document.querySelectorAll('.advisories-program')
+
+const buttonPrev = document.getElementsByClassName('btn-left')
+const buttonNext = document.getElementsByClassName('btn-rigth')
+
+/* let p: number = 0;
+
+function nextProgram() {
+	program[p].classList.remove('active')
+	p++
+
+	if(p >= program.length) { p = 0 }
+
+	program[p].classList.add('active');
+}
+ */
+
+
+let p: number = 0;
+ setInterval(function () {
+	program[p].classList.remove('active');
+	p++;
+
+	if (p >= program.length) {
+		p = 0;	
+	}
+
+	program[p].classList.add('active');
+}, 3000);
+
+
+
+
+
+
+/* Testimonials Transition */
+
+const testSlider = document.querySelector('.testimonials-slides')
+let testSlide = document.querySelectorAll('.testimonials-slide')
+let testSlideLast = testSlide[testSlide.length - 1]
+
+testSlider.insertAdjacentElement('afterbegin', testSlideLast )
+
+function nextSilder () {
+	
+	let testSlideFirst = document.querySelectorAll('.testimonials-slide')[0]
+
+	testSlider.style.marginLeft = '-200%'
+	testSlider.style.transition = 'all 1s'
+	
+	setTimeout( function() {
+		testSlider.style.transition = 'none'
+		testSlider.insertAdjacentElement('beforeend', testSlideFirst)
+		testSlider.style.marginLeft = '-100%'
+	}, 1000 )
+}
+
+setInterval( function () {
+	nextSilder()
+}, 7000)
+
+/* Toggle FAQs */
 
 const acc = document.getElementsByClassName('faqs-question');
 
@@ -21,25 +107,4 @@ for (let i = 0; i < acc.length; i++) {
 			panel.style.maxHeight = panel.scrollHeight + 'px';
 		}
 	});
-}
-
-const heroTitles = document.querySelectorAll('.hero-title');
-const heroImages = document.querySelectorAll('.hero-image');
-
-
-
-setInterval(function () {
-	heroTitles[i].classList.remove('active');
-	heroImages[i].classList.remove('active');
-	i++;
-
-	if (i >= heroTitles.length) {
-		i = 0;
-	}
-	if (i >= heroImages.length) {
-		i = 0;
-	}
-
-	heroTitles[i].classList.add('active');
-	heroImages[i].classList.add('active');
-}, 3000);
+} 
